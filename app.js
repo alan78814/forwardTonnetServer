29e28635beacae5c69eb51dd19e012d81a3e945b
alan78814 <tri78814@gmail.com>
2023-09-15 17:35:54 +0800

    feat:change listen port

--- app.js
+++ app.js
@@ -79,6 +79,6 @@ app.get('/', (req, res) => {
   res.send('Hello World');
 });
 
-app.listen(8003, () => {
-  console.log('Server running at http://localhost:8003/');
+app.listen(30000, () => {
+  console.log('Server running at http://localhost:30000/');
 });
